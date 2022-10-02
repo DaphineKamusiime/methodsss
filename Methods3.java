57d9bce29ab4a7be1cbba69adc033a0f4b133839
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-02 08:36:06 -0700

    Add files via upload

---
+++ Methods3.java
@@ -0,0 +1,8 @@
+public class Methods3{
+    static void fruit(String name, String color){
+        System.out.println(name +" is "+ color);
+    }
+    public static void main(String[] args){
+        fruit("apple","red");
+    }
+}
